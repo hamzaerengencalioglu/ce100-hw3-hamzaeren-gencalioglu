a0bfc676ab8f904aa27d1c72f7e91c21319d32bb
bayramkamus <bayramkamus@gmail.com>
2023-05-13 06:26:31 +0300

    ce100-hw3-algo-lib-cs updated

---
+++ ce100-hw3-algo-lib-cs/BellmanFord.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// @brief Bellman-Ford algorithm implementation for finding the shortest path in a graph.
+/// @details This class represents the Bellman-Ford algorithm implementation.
+public class BellmanFord
+{
+    private int verticesCount; ///< The number of vertices in the graph.
+    private List<Edge1> edges; ///< The list of edges in the graph.
+
+    /// @brief Constructs a BellmanFord object with the specified number of vertices.
+    /// @param verticesCount The number of vertices in the graph.
+    public BellmanFord(int verticesCount)
+    {
+        this.verticesCount = verticesCount;
+        edges = new List<Edge1>();
+    }
+
+    /// @brief Adds an edge to the graph.
+    /// @param source The source vertex of the edge.
+    /// @param destination The destination vertex of the edge.
+    /// @param weight The weight/cost of the edge.
+    public void AddEdge(int source, int destination, int weight)
+    {
+        edges.Add(new Edge1(source, destination, weight));
+    }
+
+    /// @brief Finds the shortest path from a source vertex to a destination vertex.
+    /// @param source The source vertex.
+    /// @param destination The destination vertex.
+    /// @return An array representing the shortest path from the source vertex to the destination vertex.
+    public int[] FindShortestPath(int source, int destination)
+    {
+        int[] distances1 = new int[verticesCount];
+        int[] previous = new int[verticesCount];
+
+        // Step 1: Initialize distances
+        for (int i = 0; i < verticesCount; i++)
+        {
+            distances1[i] = int.MaxValue;
+            previous[i] = -1;
+        }
+        distances1[source] = 0;
+
+        // Step 2: Relax edges repeatedly
+        for (int i = 0; i < verticesCount - 1; i++)
+        {
+            foreach (Edge1 edge in edges)
+            {
+                if (distances1[edge.Source] != int.MaxValue && distances1[edge.Source] + edge.Weight < distances1[edge.Destination])
+                {
+                    distances1[edge.Destination] = distances1[edge.Source] + edge.Weight;
+                    previous[edge.Destination] = edge.Source;
+                }
+            }
+        }
+
+        // Step 3: Check for negative-weight cycles
+        foreach (Edge1 edge in edges)
+        {
+            if (distances1[edge.Source] != int.MaxValue && distances1[edge.Source] + edge.Weight < distances1[edge.Destination])
+            {
+                throw new Exception("Negative-weight cycle detected. Bellman-Ford algorithm cannot handle negative-weight cycles.");
+            }
+        }
+
+        // Step 4: Reconstruct the shortest path
+        List<int> path = new List<int>();
+        int current = destination;
+        while (current != -1)
+        {
+            path.Insert(0, current);
+            current = previous[current];
+        }
+
+        return path.ToArray();
+    }
+
+    /// @brief Represents an edge in the graph.
+    private class Edge1
+    {
+        public int Source { get; } ///< The source vertex of the edge.
+        public int Destination { get; } ///< The destination vertex of the edge.
+        public int Weight { get; } ///< The weight/cost of the edge.
+
+        /// @brief Constructs an Edge object with the specified source, destination, and weight.
+        /// @param source The source vertex of the edge.
+        /// @param destination The destination vertex of the edge.
+        /// @param weight The weight/cost of the edge.
+        public Edge1(int source, int destination, int weight)
+        {
+            Source = source;
+            Destination = destination;
+            Weight = weight;
+        }
+    }
+}
+
---
+++ ce100-hw3-algo-lib-cs/pipelinesystems.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+
+public class pipelinesystems
+{
+    private int treeCount; // Declaring a private integer variable named treeCount
+    private List<Tree> trees; // Declaring a private List of Tree objects named trees
+    private List<Edge> pipeline; // Declaring a private List of Edge objects named pipeline
+
+    /// @brief Initializes a new instance of the pipelinesystems class with the specified tree count.
+    /// @param treeCount The number of trees in the pipeline system.
+    public pipelinesystems(int treeCount)
+    {
+        if (treeCount < 10)
+        {
+            throw new ArgumentException("The minimum number of trees must be 10 or more.");
+        }
+
+        this.treeCount = treeCount;
+        trees = GenerateRandomTreePositions(treeCount);
+        pipeline = new List<Edge>();
+    }
+
+    /// @brief Generates random positions for the trees in the pipeline system.
+    /// @param count The number of trees to generate positions for.
+    /// @returns A list of Tree objects with random positions.
+    private List<Tree> GenerateRandomTreePositions(int count)
+    {
+        Random random = new Random();
+        List<Tree> treeList = new List<Tree>();
+
+        for (int i = 0; i < count; i++)
+        {
+            int x = random.Next(1, 100);
+            int y = random.Next(1, 100);
+            treeList.Add(new Tree(x, y));
+        }
+
+        return treeList;
+    }
+
+    /// @brief Connects the trees in the pipeline system using the Kruskal algorithm.
+    public void ConnectTrees()
+    {
+        List<Edge> allEdges = new List<Edge>();
+
+        for (int i = 0; i < treeCount - 1; i++)
+        {
+            Tree tree1 = trees[i];
+            Tree tree2 = trees[i + 1];
+            double distance = CalculateDistance(tree1, tree2);
+            allEdges.Add(new Edge(i, i + 1, distance));
+        }
+
+        allEdges.Sort((x, y) => x.Distance.CompareTo(y.Distance));
+
+        UnionFind uf = new UnionFind(treeCount);
+        foreach (Edge edge in allEdges)
+        {
+            if (uf.Find(edge.From) != uf.Find(edge.To))
+            {
+                uf.Union(edge.From, edge.To);
+                pipeline.Add(edge);
+            }
+        }
+    }
+
+    /// @brief Gets the description of the pipeline system.
+    /// @returns A list of strings describing the pipeline connections between trees.
+    public List<string> GetPipelineDescription()
+    {
+        List<string> descriptions = new List<string>();
+        foreach (Edge edge in pipeline)
+        {
+            string description = $"Tree {edge.From} is connected to Tree {edge.To} with a pipeline of length {edge.Distance}.";
+            descriptions.Add(description);
+        }
+
+        return descriptions;
+    }
+
+    /// @brief Calculates the distance between two trees.
+    /// @param tree1 The first tree.
+    /// @param tree2 The second tree.
+    /// @returns The distance between the two trees.
+    private double CalculateDistance(Tree tree1, Tree tree2)
+    {
+        int xDiff = tree1.X - tree2.X;
+        int yDiff = tree1.Y - tree2.Y;
+        return Math.Sqrt(xDiff * xDiff + yDiff * yDiff);
+    }
+
+    /// @brief Represents a tree in the pipeline system.
+    private class Tree
+    {
+        public int X { get; }
+        public int Y { get; }
+
+        public Tree(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+
+    /// @brief Represents an edge (pipeline) between two trees in the pipeline system.
+    private class Edge
+    {
+        public int From { get; }
+        public int To { get; }
+        public double Distance { get; }
+
+        public Edge(int from, int to, double distance)
+        {
+            From = from;
+            To = to;
+            Distance = distance;
+        }
+    }
+
+    /// @brief Represents a disjoint-set data structure used in the Kruskal algorithm.
+    private class UnionFind
+    {
+        private int[] parent;
+        private int[] rank;
+
+        public UnionFind(int size)
+        {
+            parent = new int[size];
+            rank = new int[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                parent[i] = i;
+                rank[i] = 0;
+            }
+        }
+        // Defining a public method named Find that takes an integer argument named x and returns an integer
+        public int Find(int x)
+        {
+            if (parent[x] != x)
+            {
+                parent[x] = Find(parent[x]);
+            }
+
+            return parent[x];
+        }
+
+        public void Union(int x, int y)// Defining a public method named Union that takes two integer arguments named x and y and returns nothing
+        {
+            // Calling the Find method with x and y as an argument and assigning its return value to the rootX and rootY variable
+            int rootX = Find(x);
+            int rootY = Find(y);
+            // Checking if the element at index rootX in the rank array is less than the element at index rootY in the rank array
+            if (rank[rootX] < rank[rootY])
+            {
+                parent[rootX] = rootY;
+            }
+            // If it is less, assigning the value of rootY to the element at index rootX in the parent array
+            else if (rank[rootX] > rank[rootY])
+            {
+                parent[rootY] = rootX;
+            }
+            // If neither of the previous conditions are true
+            else
+            {
+                parent[rootY] = rootX;
+                rank[rootX]++;
+            }
+        }
+    }
+}
+
+
+
