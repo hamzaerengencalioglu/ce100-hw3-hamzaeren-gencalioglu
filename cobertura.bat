47fd1a2bf42014a36b4198e226426f7677d41124
hamzaerengencalioglu <hamzaerengencalioglu@gmail.com>
2023-05-13 21:46:35 +0300

    cobertura.bat deleted

--- cobertura.bat
+++
@@ -1,5 +0,0 @@
-dotnet test --verbosity normal --collect:"XPlat Code Coverage"
-reportgenerator "-reports:**/coverage.cobertura.xml" "-targetdir:coveragereport" -reporttypes:Html
-reportgenerator "-reports:**/coverage.cobertura.xml" "-targetdir:assets" -reporttypes:Badges
-start coveragereport\index.html
-pause
